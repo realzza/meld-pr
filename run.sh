f89f846bd096098cb03250861384400f0df464d6
Ziang Zhou <ziangz@andrew.cmu.edu>
2022-11-17 01:53:17 -0500

    update entry script

--- run.sh
+++ run.sh
@@ -5,9 +5,31 @@ set -e
 set -u
 set -o pipefail
 
+train_set="train"
+valid_set="valid"
+test_sets="valid test"
+
+asr_config=conf/tuning/train_asr_hubert_transformer_adam_specaug_meld.yaml
+inference_config=conf/decoder_asr.yaml
+
 ./asr.sh \
     --lang en \
-    --train_set train_nodev \
-    --valid_set train_dev \
-    --test_sets "train_dev test test_seg" \
-    --lm_train_text "data/train_nodev/text" "$@"
+    --ngpu 1 \
+    --nj 8 \
+    --stop_stage 13 \
+    --inference_config "${inference_config}" \
+    --use_lm false \
+    --nbpe 850 \
+    --token_type bpe\
+    --bpe_train_text "dump/raw/train/text" \
+    --bpe_nlsyms sadness,surprise,neutral,joy,anger,fear,disgust\
+    --audio_format wav\
+    --feats_type raw\
+    --max_wav_duration 20 \
+    --feats_normalize utterance_mvn\
+    --inference_nj 4 \
+    --inference_asr_model valid.acc.ave_5best.pth\
+    --asr_config "${asr_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" "$@"
